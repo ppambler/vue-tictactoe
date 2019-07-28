a6dbf4d35bf0ae5630a0f0992eeed2d7b5a36e50
Ambler <allinlbj@gmail.com>
2019-07-28 12:51:43 +0800

    使用eventBus完成了上一次点击是x，下一次点击是○

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,46 @@
 <template>
   <div>
     <div class="row">
-      <Cell />
-      <Cell />
-      <Cell />
+      <Cell name="cell1"/>
+      <Cell name="cell2"/>
+      <Cell name="cell3"/>
     </div>
     <div class="row">
-      <Cell />
-      <Cell />
-      <Cell />
+      <Cell name="cell4"/>
+      <Cell name="cell5"/>
+      <Cell name="cell6"/>
     </div>
     <div class="row">
-      <Cell />
-      <Cell />
-      <Cell />
+      <Cell name="cell7"/>
+      <Cell name="cell8"/>
+      <Cell name="cell9"/>
     </div>
   </div>
 </template>
 
 <script>
 import Cell from "./Cell";
+import Vue from "vue"
 export default {
   name: "app",
   components: {
     Cell
-  }
+  },
+  data() {
+    return {
+      eventBus: new Vue(),
+    }
+  },
+  provide() {
+    return {
+      eventBus: this.eventBus
+    }
+  },
+  created() {
+    this.eventBus.$on('update:has',function(x) {
+      // console.log('你有拿到值吗？')
+    })
+  },
 };
 </script>
 
--- src/Cell.vue
+++ src/Cell.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="cell" v-on:click="a = true">
+    <div class="cell" v-on:click="xxx">
       <template v-if="a">{{text}}</template>
       <template v-else></template>
     </div>
@@ -8,12 +8,41 @@
 <script>
 export default {
   name: "cell",
+  inject: ['eventBus'],
+  props: {
+    name: {
+      type: String
+    }
+  },
   data() {
     return {
       a: false,
-      text: 'x'
+      text: 'x',
+      temp: false
     }
-  }
+  },
+  created() {
+    // console.log(this.name)
+    let _this = this
+    this.eventBus.$on('update:has',function(n) {
+      // 这个事件的监听先于父组件的监听执行
+      // console.log(this) //这个this是eventBus 这个Vue实例呀！
+      // console.log(_this)
+      // console.log(`${_this.name}说「${n}」被点击了`)
+      _this.temp = n
+    })
+  },
+  methods: {
+    xxx() {
+      if(this.temp) {
+        this.text = '○'
+        this.a = true
+      }
+      this.a = true
+      this.eventBus.$emit('update:has', !this.temp)
+    }
+  },
+
 };
 </script>
 
